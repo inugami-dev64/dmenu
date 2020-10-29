9c540a29ddda40e3ed62dff48b4a2b07fbc7d030
inugami-dev64 <jxwest@protonmail.com>
2020-10-29 17:36:51 +0200

    Initial commit

--- config.h
+++ config.h
@@ -10,14 +10,14 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#f18eab" },
+	[SchemeSelHighlight] = { "#bb0000", "#f18eab" },
+	[SchemeNormHighlight] = { "#bb0000", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-static unsigned int lineheight = 0;         /* -h option; minimum height of a menu line     */
+static unsigned int lineheight = 20;         /* -h option; minimum height of a menu line     */
 
 /*
  * Characters not considered part of a word while deleting words
