645ffbd94a65d53cbd2f1febf87803cc5cbe538a
inugami-dev64 <jxwest@protonmail.com>
2021-05-21 21:19:42 +0300

    Windows 95 style rice

--- config.h
+++ config.h
@@ -4,20 +4,25 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Source Code Pro:size=11"
+	"W95FA:size=12"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt           = NULL;      /* -p  option; prompt to the left of input field */
+static const char blue[]            = "#000080";
+static const char green[]           = "#00cc00";
+static const char gray[]            = "#c0c0c0";
+static const char white[]           = "#ffffff";  
+static const char black[]           = "#000000";
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#f18eab" },
-	[SchemeSelHighlight] = { "#bb0000", "#f18eab" },
-	[SchemeNormHighlight] = { "#bb0000", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { black, gray },
+	[SchemeSel] = { white, blue },
+	[SchemeSelHighlight] = { green, blue },
+	[SchemeNormHighlight] = { green, gray },
+	[SchemeOut] = { black, blue },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-static unsigned int lineheight = 20;         /* -h option; minimum height of a menu line     */
+static unsigned int lineheight = 30;         /* -h option; minimum height of a menu line     */
 
 /*
  * Characters not considered part of a word while deleting words
